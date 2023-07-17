1206b7318923b521c3c00f2b8814959055192193
Geree <smithzero94@gmail.com>
2023-07-17 18:49:33 +0200

    Book class, Reader class implemented, yet to be improved with few methods

--- src/Book.java
+++ src/Book.java
@@ -1,11 +1,23 @@
+import java.util.HashSet;
 import java.util.Set;
 
 public class Book {
     private Set<String> authors;
-    private String author;
     private String title;
     private int releaseYear;
     private Genre genre;
+
+    public Book(String author, String title, int releaseYear, Genre genre) {
+        this.authors = new HashSet<>();
+        this.authors.add(author);
+        this.title = title;
+        this.releaseYear = releaseYear;
+        this.genre = genre;
+    }
+
+    public void addAuthor(String author) {
+        this.authors.add(author);
+    }
 
 
 }
--- src/Reader.java
+++ src/Reader.java
@@ -1,2 +1,35 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Reader {
+    private String name;
+    private int readerTicket;
+    private Set<Genre> favoriteGenres;
+
+    public Reader(String name, int readerTicket, Genre favorieGenre) {
+        this.name = name;
+        this.readerTicket = readerTicket;
+        this.favoriteGenres = new HashSet<>();
+        this.favoriteGenres.add(favorieGenre);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getReaderTicket() {
+        return readerTicket;
+    }
+
+    public Set<Genre> getFavoriteGenres() {
+        return favoriteGenres;
+    }
+
+    public void addGenre(Genre genre) {
+        this.favoriteGenres.add(genre);
+    }
+
+    public void removeGenre(Reader reader, Genre genre) {
+
+    }
 }
