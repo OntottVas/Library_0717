ba547b562a3b6c84804c73842cd7df151f959132
Geree <smithzero94@gmail.com>
2023-07-19 19:24:31 +0200

    Book class improved

--- src/Book.java
+++ src/Book.java
@@ -2,22 +2,53 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Book {
-    private Set<String> authors;
+    private final Set<String> authors = new HashSet<>();
     private String title;
-    private int releaseYear;
-    private Genre genre;
+    private final int releaseYear;
+    private final Genre genre;
 
     public Book(String author, String title, int releaseYear, Genre genre) {
-        this.authors = new HashSet<>();
         this.authors.add(author);
         this.title = title;
         this.releaseYear = releaseYear;
         this.genre = genre;
     }
 
+    public Set<String> getAuthors() {
+        return authors;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
     public void addAuthor(String author) {
         this.authors.add(author);
     }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "authors=" + authors +
+                ", title='" + title + '\'' +
+                ", releaseYear=" + releaseYear +
+                ", genre=" + genre +
+                '}';
+    }
+
+
 
 
 }
